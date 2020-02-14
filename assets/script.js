904c5a5872592793576b8f66ab96be3ba8e20c33
vkaszuba <codeword.vika@gmail.com>
2020-02-14 01:03:45 -0800

    Linking user input location to reflect on API query, clicking button now runs the API function

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,38 @@
-let userCity = "Berlin";
-let userState = "California";
-
-const APIKey = "166a433c57516f51dfab1f7edaed8413";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${userCity},${userState}&units=imperial&appid=${APIKey}`;
 
 
+$("#getWeather").click(function (event) {
+  event.preventDefault();
 
-$.ajax({
-  url,
-  method: "GET"
-}).then(function(response){
+  let userCity = $("#getLocation").val();
 
-  let uvUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+  const APIKey = "166a433c57516f51dfab1f7edaed8413";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=imperial&appid=${APIKey}`;
 
   $.ajax({
+    url,
+    method: "GET"
+  }).then(function (response) {
+
+    let uvUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${APIKey}`;
+
+    $.ajax({
       url: uvUrl,
       method: "GET"
-  }).then(function(uvResponse){
+    }).then(function (uvResponse) {
 
-  console.log(response);
-  console.log(uvResponse);
+      let date = new Date(response.dt * 1000).toDateString();
+      let icon = `http://openweathermap.org/img/wn/${response.weather["0"].icon}@2x.png`;
 
-  let date = new Date(response.dt * 1000).toDateString();
-  let icon = `http://openweathermap.org/img/wn/${response.weather["0"].icon}@2x.png`;
+      $(".city").text(`City Name: ${response.name + ", " + response.sys.country}`);
+      $(".date").text(`Date: ${date}`);
+      $(".icon").attr("src", icon);
+      $(".temp").text(`Temperature: ${response.main.temp}`);
+      $(".wind").text(`Wind Speed: ${response.wind.speed}`);
+      $(".uvIndex").text(`UV Index: ${uvResponse.value}`);
+      $(".humidity").text(`Humidity: ${response.main.humidity}`);
 
-  $(".city").text(`City Name: ${response.name + ", " + response.sys.country}`);
-  $(".date").text(`Date: ${date}`);
-  $(".icon").attr("src", icon);
-  $(".temp").text(`Temperature: ${response.main.temp}`);
-  $(".wind").text(`Wind Speed: ${response.wind.speed}`);
-  $(".uvIndex").text(`UV Index: ${uvResponse.value}`);
-  $(".humidity").text(`Humidity: ${response.main.humidity}`);
+    })
+  });
 
 
-
-})
-
-console.log(response);
-console.log(uvResponse);
 });
-
