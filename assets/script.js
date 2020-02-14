e7e271c00902864f9a4d5da56ee0ea6b0c83dfcf
vkaszuba <codeword.vika@gmail.com>
2020-02-14 00:09:12 -0800

    Linking all needed API info to the HTML page, API setup

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,42 @@
-// This is our API key. Add your own API key between the ""
-const APIKey = "166a433c57516f51dfab1f7edaed8413";
-
-// San+Francisco,California
 let userCity = "Berlin";
 let userState = "California";
 
-// Here we are building the URL we need to query the database
-let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userCity},${userState}&units=imperial&appid=${APIKey}`;
+const APIKey = "166a433c57516f51dfab1f7edaed8413";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${userCity},${userState}&units=imperial&appid=${APIKey}`;
 
-// We then created an AJAX call
+
+
 $.ajax({
-  url: queryURL,
+  url,
   method: "GET"
-}).then(function(response) {
+}).then(function(response){
 
+  let uvUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${response.coord.lat}&lon=${response.coord.lon}`;
 
+  $.ajax({
+      url: uvUrl,
+      method: "GET"
+  }).then(function(uvResponse){
 
   console.log(response);
-  console.log(response.wind.speed);
-  console.log(response.sys.country);
-  let windSpeed = response.wind.speed;
-  let temperature = response.main.temp;
-  let cityName = response.name + ", " + response.sys.country;
+  console.log(uvResponse);
+
+  let date = new Date(response.dt * 1000).toDateString();
+  let icon = `http://openweathermap.org/img/wn/${response.weather["0"].icon}@2x.png`;
+
+  $(".city").text(`City Name: ${response.name + ", " + response.sys.country}`);
+  $(".date").text(`Date: ${date}`);
+  $(".icon").attr("src", icon);
+  $(".temp").text(`Temperature: ${response.main.temp}`);
+  $(".wind").text(`Wind Speed: ${response.wind.speed}`);
+  $(".uvIndex").text(`UV Index: ${uvResponse.value}`);
+  $(".humidity").text(`Humidity: ${response.main.humidity}`);
 
 
- $(".city").text(`City Name: ${cityName}`);
-  $(".wind").text(`Wind Speed: ${windSpeed}`);
-  $(".temp").text(`Temperature: ${temperature}`);
-  
 
+})
 
-  
-
-  // Create CODE HERE to Log the queryURL
-  // Create CODE HERE to log the resulting object
-  // Create CODE HERE to transfer content to HTML
-  // Create CODE HERE to calculate the temperature (converted from Kelvin)
-  // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-  // Create CODE HERE to dump the temperature content into HTML
-
+console.log(response);
+console.log(uvResponse);
 });
+
